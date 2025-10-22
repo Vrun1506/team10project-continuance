214fd5c5a463e1d44ca7e857496ab0ea322e5781
BenLeaver <benjackleaver@hotmail.com>
2025-10-22 09:35:20 +0100

    Create Room class

---
+++ Game/core/src/main/java/io/github/team10/escapefromuni/Room.java
@@ -0,0 +1,15 @@
+package io.github.team10.escapefromuni;
+
+public class Room {
+    public RoomManager roomManager;
+    public Event event;
+    public Room[] adjacentRooms = new Room[4];
+
+    public EventType getEventType() {
+        // Return the event type of event
+    }
+
+    public void start() {
+        // Actions to perform when room is started i.e. play event
+    }
+}
