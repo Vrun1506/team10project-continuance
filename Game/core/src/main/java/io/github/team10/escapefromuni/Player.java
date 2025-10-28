b583fd11252bd2d159faeb75c940c3809b68ebde
BenLeaver <benjackleaver@hotmail.com>
2025-10-28 23:30:34 +0000

    Update player movement
    Add walls to prevent player moving to close to edge. Center player initially.

--- Game/core/src/main/java/io/github/team10/escapefromuni/Player.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/Player.java
@@ -11,13 +11,19 @@ public class Player {
     public float speed;
     public EscapeGame game;
 
-    public Player(float speed, EscapeGame game)
+    private final float EDGE_LIMIT = 1f;
+
+    public Player(float speed, float playerWidth, float playerHeight, EscapeGame game)
     {
         this.speed = speed;
         this.game = game;
         playerTexture = new Texture("Player.png");
         playerSprite = new Sprite(playerTexture);
-        playerSprite.setSize(1, 1);
+        playerSprite.setSize(playerWidth, playerHeight);
+
+        float centerX = game.viewport.getWorldWidth() / 2f;
+        float centerY = game.viewport.getWorldHeight() / 2f;
+        playerSprite.setCenter(centerX, centerY);
     }
 
     public void update(float delta)
@@ -26,19 +32,56 @@ public class Player {
     }
 
     private void move(float delta) {
+        float worldWidth = game.viewport.getWorldWidth();
+        float worldHeight = game.viewport.getWorldHeight();
+        float halfWidth = playerSprite.getWidth() / 2f;
+        float halfHeight = playerSprite.getHeight() / 2f;
+        float playerCenterX = playerSprite.getX() + halfWidth;
+        float playerCenterY = playerSprite.getY() + halfHeight;
+
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            playerSprite.translateX(speed * delta);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            playerSprite.translateX(-speed * delta);
+            if (playerCenterX < worldWidth - EDGE_LIMIT) {
+                playerSprite.translateX(speed * delta);
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            if (playerCenterX > EDGE_LIMIT) {
+                playerSprite.translateX(-speed * delta);
+            }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            playerSprite.translateY(speed * delta);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            playerSprite.translateY(-speed * delta);
+            if (playerCenterY < worldHeight - EDGE_LIMIT) {
+                playerSprite.translateY(speed * delta);
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            if (playerCenterY > EDGE_LIMIT) {
+                playerSprite.translateY(-speed * delta);
+            }
         }
     }
 
     public void draw() {
         playerSprite.draw(game.batch);
+    }
+
+    public boolean checkCollision(Sprite objectSprite)
+    {
+        return playerSprite.getBoundingRectangle().overlaps(objectSprite.getBoundingRectangle());
+    }
+
+    public void dispose()
+    {
+        playerTexture.dispose();
+    }
+
+    public void setPosition(float x, float y)
+    {
+        playerSprite.setPosition(x, y);
+    }
+
+    public void setCenter(float x, float y)
+    {
+        playerSprite.setCenter(x, y);
     }
 }
