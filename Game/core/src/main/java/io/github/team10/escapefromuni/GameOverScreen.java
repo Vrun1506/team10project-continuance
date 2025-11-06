f8229127c6fcfa14fc848f40610114991ecefafd
chiiiii123 <161492942+chiiiii123@users.noreply.github.com>
2025-11-06 14:40:52 +0000

    Create GameOverScreen.java

---
+++ Game/core/src/main/java/io/github/team10/escapefromuni/GameOverScreen.java
@@ -0,0 +1,33 @@
+package io.github.team10.escapefromuni;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+public class GameOverScreen implements Screen {
+
+    private final EscapeGame game;
+    private BitmapFont font;
+
+    public GameOverScreen(final EscapeGame game) {
+        this.game = game;
+        font = new BitmapFont();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        font.draw(game.batch, "GAME OVER", 200, 250);
+        font.draw(game.batch, "Press ESC or close window", 200, 200);
+        game.batch.end();
+    }
+
+    @Override public void show() {}
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+    @Override public void dispose() { font.dispose(); }
+}
